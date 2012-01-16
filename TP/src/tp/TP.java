2c4b5a3a1a9da2fca02d0cc9d876ca20fb2a7c5a
Illia Izotov <izotov.ilya@gmail.com>
2012-01-16 14:47:47 +0100

    Created project

---
+++ TP/src/tp/TP.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp;
+
+/**
+ *
+ * @author FeLiX
+ */
+public class TP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
