0e0f823f144872f4c366f108e2c8e04654f2cfe6
Illia Izotov <izotov.ilya@gmail.com>
2012-01-16 15:42:42 +0100

    Created project

--- TP/src/tp/MainFrame.java
+++ TP/src/tp/MainFrame.java
@@ -11,7 +11,7 @@ package tp;
 public class MainFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form MainFrame
+     * Creates new form MainFrame test
      */
     public MainFrame() {
         initComponents();
