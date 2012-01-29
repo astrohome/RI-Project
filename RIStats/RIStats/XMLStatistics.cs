d26904a5f5678d401b497d00f9ec21b58360c715
Illia Izotov <izotov.ilya@gmail.com>
2012-01-29 23:44:32 +0100

    Have done everything, that I could. Still a lot of work for XML.
    Signed-off-by: Illia Izotov <izotov.ilya@gmail.com>

--- RIStats/RIStats/XMLStatistics.cs
+++ RIStats/RIStats/XMLStatistics.cs
@@ -17,23 +17,28 @@ namespace RIStats
 
     public class XMLStatistics
     {
-        public Dictionary<Int32, List<Data>> tf = new Dictionary<int, List<Data>>(); 
+        //                Doc               Path                Word   Tf
+        public Dictionary<Int32, Dictionary<String, Dictionary<String, Int32>>> tf = new Dictionary<Int32, Dictionary<String, Dictionary<String, Int32>>>(); 
 
         public Dictionary<String, Int32> tag_df = new Dictionary<string, int>();
         public Dictionary<String, Int32> word_df = new Dictionary<string, int>();
         public List<String> words = new List<string>();
-        public List<String> tags = new List<string>();
+        public Dictionary<String, Int32> tags = new Dictionary<String, Int32>();
+
+        private Dictionary<String, Dictionary<String, Int32>> tags_df = new Dictionary<string, Dictionary<string, int>>();
 
         public Dictionary<string, Int32> balises = new Dictionary<string, int>();
         public System.Collections.ArrayList al = new System.Collections.ArrayList();
 
         private Int32 _currentDocument;
         private String _currentPath;
+        public Int32 DocumentsNumber = 0;
 
         public void Proceed(String url)
         {
             string[] filePaths = Directory.GetFiles(url, "*.xml");
             string last = "last";
+
             foreach (var file in filePaths)
             {
                 _currentDocument = Convert.ToInt32(Path.GetFileNameWithoutExtension(file));
@@ -57,7 +62,8 @@ namespace RIStats
                                 last += "[" + k + "]";
                                 al.Add(last);
                                 _currentPath = last;
-                                if (!tags.Contains(reader.Name)) tags.Add(reader.Name);
+                                
+                                if (!tags.ContainsKey(reader.Name)) tags.Add(reader.Name, 1);
                             }
                             break;
                         case XmlNodeType.Text:
@@ -79,11 +85,28 @@ namespace RIStats
                             break;
                     }
                 }
+
+                al.Clear();
+                balises.Clear();
+                DocumentsNumber++;
+                Console.WriteLine("Finished " + DocumentsNumber);
             }
+        }
+
+        public void Proceedltn()
+        {
+
         }
 
         private void ProceedLine(string line)
         {
+            // converting the path into a string
+            string path = "";
+            for (int j = 0; j < al.Count; j++)
+                path += "/" + al[j];
+
+            _currentPath = path;
+
             var wordsline = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             //forech trim symbols to get real words and make everything in lower register
             foreach (var w in wordsline.Select(word => Regex.Replace(word, "[^a-zA-Z0-9_]+", "", RegexOptions.Compiled)).Select(w => w.ToLower()))
@@ -95,15 +118,25 @@ namespace RIStats
 
                 if (!tf.ContainsKey(_currentDocument))
                 {
-                    tf.Add(_currentDocument, new List<Data>());
+
+                    tf.Add(_currentDocument, new Dictionary<string,Dictionary<string,int>>());
                 }
                 else
                 {
-                    foreach (var st in tf[_currentDocument])
+                    if (tf[_currentDocument].ContainsKey(_currentPath))
                     {
-                        if (st.path == _currentPath)
+                        if (tf[_currentDocument][_currentPath].ContainsKey(w))
                         {
+                            tf[_currentDocument][_currentPath][w]++;
                         }
+                        else
+                        {
+                            tf[_currentDocument][_currentPath].Add(w, 1);
+                        }
+                    }
+                    else
+                    {
+                        tf[_currentDocument].Add(_currentPath, new Dictionary<string, int>() { { w, 1 } });
                     }
                 }
             }
